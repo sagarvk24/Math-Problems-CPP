26c5b91249c6e487ef7c2eccd22d3e09bbc954c5
sagarvk24 <sagarsaan21@gmail.com>
2025-01-06 12:53:29 +0530

    code added for count number of odd digits in a number

---
+++ 2. Count number of odd digits in a number/optimal.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int countOddDigits(int &num)
+{
+    if (num == 0)
+        return 1;
+    int oddCount = 0;
+
+    while (num > 0)
+    {
+        int lastDigit = num % 10;
+        if (lastDigit % 2 != 0)
+            oddCount += 1;
+        num /= 10;
+    }
+
+    return oddCount;
+}
+
+int main()
+{
+    int number;
+    cout << "Enter any number: ";
+    cin >> number;
+
+    int count = countOddDigits(number);
+    cout << "The number of odd digits in the number are = " << count;
+
+    return 0;
+}
